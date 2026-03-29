9ee4004f1c1f3cb0e4378b89ba007197734d4bc2
Santhosh R <santhosh30072006@gmail.com>
2026-03-29 22:32:27 +0530

    Switch to gemini-1.5-flash

--- chatbot1.py
+++ chatbot1.py
@@ -27,7 +27,7 @@ you should answer them in polite, if there is any questions out of the kb say yo
 # --- Init Gemini ---
 st.session_state.client = genai.Client(api_key=st.secrets["GEMINI_API_KEY"])
 st.session_state.chat = st.session_state.client.chats.create(
-    model="gemini-2.0-flash",
+    model="gemini-1.5-flash",
     config={"system_instruction": system_prompt}
 )
 
